45cb19d684388553813fdf8ca052ed536d46d899
Franco Sbaffi <francosbaffidev@gmail.com>
2024-11-29 21:28:02 -0300

    Se agregaron muchos cambios (Funciona NGINX, app1 y app2)

---
+++ docker/app/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Verificar si la variable de entorno PORT está configurada, si no, usar el valor predeterminado 3000
+PORT=${PORT:-3000}
+
+echo "Configurando Apache para escuchar en el puerto ${PORT}..."
+
+# Cambiar el puerto en ports.conf
+sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+
+# Cambiar el puerto en el sitio por defecto
+sed -i "s/<VirtualHost \*:80>/<VirtualHost \*:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+
+# Habilitar el sitio por defecto
+a2ensite 000-default.conf
+
+# Establecer el ServerName para evitar advertencias
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+echo "Iniciando el servidor Apache en el puerto ${PORT}..."
+exec "$@"
+
+
+
